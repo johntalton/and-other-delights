16327495e137a0275ff507f61783e62a79260db9
johntalton <johntalton@gmail.com>
2020-05-09 04:10:54 +0000

    and ref

--- src/aod.js
+++ src/aod.js
@@ -4,7 +4,8 @@ const { BusUtil } = require('./busutil.js');
 const { ConfigUtil } = require('./configutil.js');
 const { NameValueUtil } = require('./nvutil.js');
 const { ClassSelector } = require('./classselector.js');
+const { I2CAddressableBus } = require('./i2c.js');
 
 module.exports = {
-  BitUtil, BusUtil, ConfigUtil, NameValueUtil, ClassSelector
+  BitUtil, BusUtil, ConfigUtil, NameValueUtil, ClassSelector, I2CAddressedBus
 };

7ba3904cadb503132482d0e743473bd71e78d569
johntalton <johntalton@gmail.com>
2021-03-09 08:17:17 -0500

    re-expore ... eol soon-ish

--- src/aod.mjs
+++ src/aod.mjs
@@ -2,7 +2,7 @@
 export * from '../lib/bitutil.js'
 export * from '../lib/busutil.js'
 export * from '../lib/i2c-addressed.js'
-//export * from '../lib/i2c-mock.js'
+export * from '../lib/i2c-mock.js'
 export * from '../lib/i2c-scriptbus.js'
-//export * from '../lib/i2c-throwbus.js'
+export * from '../lib/i2c-throwbus.js'
 // # sourceMappingURL=../lib/aod.js.map

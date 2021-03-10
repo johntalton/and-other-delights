75176074107f46ee44c70952f911876271e06ae8
johntalton <johntalton@gmail.com>
2021-03-09 23:15:42 -0500

    restored

---
+++ src/aod.ts
@@ -0,0 +1,10 @@
+/* eslint-disable spellcheck/spell-checker */
+// main entry point into this library
+export * from './i2c.js'
+
+export * from './bitutil.js'
+export * from './busutil.js'
+export * from './i2c-addressed.js'
+export * from './i2c-mock.js'
+export * from './i2c-scriptbus.js'
+export * from './i2c-throwbus.js'
